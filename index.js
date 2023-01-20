c858017e55ce555d9e1ab62616029d919ac58a67
Leroy Mataa-Hewa Abiguime <mataa.abiguime17@gmail.com>
2023-01-20 00:21:04 +0000

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const app = express(); 
+const path = require('path'); 
+const router = express.Router(); 
+// Setup essential routes 
+app.get('/', function(req, res) { 
+    res.send("Welecome")
+ 
+}); 
+app.get('/about', function(req, res) { 
+   res.send("about")
+}); 
+app.get('/sitemap', function(req, res) { 
+    res.send("sitemap")
+}); 
+//add the router 
+app.use('/', router); 
+app.listen(process.env.port || 3000); 
+console.log('Running at Port 3000'); 
